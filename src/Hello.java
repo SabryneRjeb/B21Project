74ecfd80a96485d737e85eacd187b2a955b298cb
SabryneRjeb <sabryne.rjeb@gmail.com>
2021-01-02 18:28:01 -0500

    Project is shared in remote repo committing now

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("New class for practice");
+        // I have already published on GitHub.com
+        // now i am ready to make more commits
+        // but do i need it need to share again??
+        // ABSOLUTELY no
+        // IT's Time commit and just PUSH!!!
     }
 }
